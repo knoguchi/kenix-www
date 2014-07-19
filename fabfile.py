04733029db5eebcc07e7b20bbfa29f52532ea09f
Kenji Noguchi <kenji@dell.local>
2014-07-19 11:19:15 -0700

    add build deps

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,7 @@
-import unittest
+import os
 from fabric.api import env, local, task
 
+PROJECT_ROOT=os.path.abspath(os.path.dirname(__file__))
 @task
 def run():
     local('dev_appserver.py .')
@@ -9,4 +10,8 @@ def test():
     local('nosetests tests')
 @task
 def collectdeps():
+    ONE_LEVEL_ABOVE = os.path.abspath(os.path.join(PROJECT_ROOT, '..'))
+    local('cd %s && rm -rf endpoints-proto-datastore && git clone https://github.com/GoogleCloudPlatform/endpoints-proto-datastore' % ONE_LEVEL_ABOVE)
+    local('mkdir -p %s/libs' % PROJECT_ROOT)
+    local('cd %s/libs && rm endpoints_proto_datastore && ln -s ../../endpoints-proto-datastore/endpoints_proto_datastore' % PROJECT_ROOT)
     local('python manage.py collectdeps -r requirements.txt')
